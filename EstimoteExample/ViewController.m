3c5cf857964f0695361725839f524c0ade372520
nvohra <nvohra@paypal.com>
2015-10-24 16:29:11 -0500

    adding simplify commerce

--- EstimoteExample/ViewController.m
+++ EstimoteExample/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <Simplify/SIMChargeCardViewController.h>
 
-@interface ViewController () <ESTIndoorLocationManagerDelegate>
+@interface ViewController () <ESTIndoorLocationManagerDelegate, SIMChargeCardViewControllerDelegate>
 
 @property (nonatomic) ESTIndoorLocationManager *locationManager;
 @property (nonatomic) ESTLocation *location;
@@ -36,6 +37,26 @@
         NSLog(@"can't fetch location: %@", error);
     }];
     
+    
+    //4. Add SIMChargeViewController to your view hierarchy
+    
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:true];
+    PKPaymentRequest* paymentRequest = [[PKPaymentRequest alloc] init];
+    paymentRequest.merchantIdentifier = @"merchant.com.velle.money2020";
+    paymentRequest.merchantCapabilities = PKMerchantCapabilityEMV | PKMerchantCapability3DS;
+    paymentRequest.supportedNetworks = @[PKPaymentNetworkAmex, PKPaymentNetworkMasterCard, PKPaymentNetworkVisa];
+    paymentRequest.countryCode = @"US";
+    paymentRequest.currencyCode = @"USD";
+    SIMChargeCardViewController *chargeVC = [[SIMChargeCardViewController alloc] initWithPublicKey:@"sbpb_NjhjODgwNTctNDVkYS00MzNmLWFmZDgtYzkzMTcyZTgwZmZl" paymentRequest:paymentRequest];
+    
+    //3. Assign your class as the delegate to the SIMChargeViewController class which takes the user input and requests a token
+    chargeVC.delegate = self;
+
+    [self presentViewController:chargeVC animated:YES completion:nil];
+
 }
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -63,6 +84,27 @@ didFailToUpdatePositionWithError:(NSError *)error {
     }
     NSLog(@"x: %5.2f, y: %5.2f, orientation: %3.0f, accuracy: %@",
           position.x, position.y, position.orientation, accuracy);
+}
+
+
+-(void)creditCardTokenProcessed:(SIMCreditCardToken *)token {
+    
+    //Process the provided token
+    NSLog(@"Token:%@", token.token);
+    
+}
+
+-(void)chargeCardCancelled {
+    
+    //User cancelled the SIMChargeCardViewController
+    NSLog(@"User Cancelled");
+    
+}
+
+-(void)creditCardTokenFailedWithError:(NSError *)error {
+    
+    NSLog(@"Credit Card Token Failed with error:%@", error.localizedDescription);
+   
 }
 
 
